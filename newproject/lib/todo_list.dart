f2ab39635b6ea73c44885bb3115c4919d21623cb
keane032 <keane032@gmail.com>
2019-10-19 17:22:31 -0300

    create proj

---
+++ newproject/lib/todo_list.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:newproject/todo.dart';
+
+class TodoList extends StatefulWidget {
+
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+   List<Todo> todos = [];
+  
+  _toggleTodo(Todo todo, bool isChecked) {
+     setState(() {
+       todo.isDone = isChecked;
+     });
+   }
+
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+    return CheckboxListTile(
+      value: todo.isDone,
+       title: Text(todo.title),
+       onChanged: (bool isChecked) {
+         _toggleTodo(todo, isChecked);
+       },
+    );
+   }
+
+    _addTodo() {
+     showDialog(
+       context: context,
+       builder: (BuildContext context) {
+         return AlertDialog(
+           title: Text('New todo'),
+           content: TextField(),
+         );
+       },
+     );
+   }
+
+    @override
+    Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(title: Text('Todo List')),
+       body: ListView.builder(
+         itemBuilder: _buildItem,
+         itemCount: todos.length,
+       ),
+       floatingActionButton: FloatingActionButton(
+         child: Icon(Icons.add),
+         onPressed: _addTodo,
+       ),
+      );
+ 
+}
+}
